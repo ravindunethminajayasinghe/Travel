993b98a4ac55d4cf961be500a59b37cfd87335a7
ravindunethminajayasinghe <ghravindunethminajayasinghe@gmail.com>
2025-11-05 00:53:08 +0530

    BrowserRouter changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DistrictPage from './pages/DistrictPage';
 import './App.css'; // You'll use this for styling
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/district/:districtName" element={<DistrictPage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
